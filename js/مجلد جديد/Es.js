19deef6dd01e673cd3072b064922af946e883b6c
root**44.2000 <pythonh37@gmail.com>
2021-06-06 21:02:31 +0300

    creat js

---
+++ js/مجلد جديد/Es.js
@@ -0,0 +1,13 @@
+/*let myMap = new Map([
+    ["10","string"],
+    [10,"Number"],
+    [10,"Number"],
+    [false,"Boolen"],
+    [function myinfo(){},"function"]
+]);
+console.log(myMap);*/
+var user ={};
+user["username"]="Osama";
+user["10"]="string";
+
+console.log(user);
---
+++ js/مجلد جديد/pro.js
@@ -0,0 +1,27 @@
+/*The Fairst Function*/
+
+function SayHi(){
+    "use strict";
+   var theDate =new Date();
+   var thenewDate=theDate.toString();
+   console.log(thenewDate);
+
+   
+ 
+ 
+       
+ }
+
+   
+  
+    
+   // SayHi();
+   SayHi();
+
+/*(function sayhello(){
+   "use strict";
+    var myageinday =21 * 365;
+   return alert("Hello mr Mohammed Adham your age in day ="+ myageinday +" Days");
+   
+})();*/
+
